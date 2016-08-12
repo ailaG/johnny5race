49879e03e1bcf0378442e227fcdff5b7aa352982
חן עידן < >
2016-08-13 02:30:14 +0300

    CSS okay. Merge may have f***ed things up.

--- client/scripts/game.js
+++ client/scripts/game.js
@@ -6,10 +6,15 @@ if(Math.random()>0.5){
  	document.getElementById("team").innerHTML = "Your team is: B";
 
  }
+ console.log("hello");
+  $(".adder").click(function(){
+       $.ajax({url: "http://dev.galiaba.com/chen.php",
   $("button").click(function(){
        $.ajax({
        		url: "http://dev.galiaba.com/chen.php",
        		success: function(result){
+           		$("#team").html(result);
+        	}});
            		$("#div1").html(result);
         	}
         });
