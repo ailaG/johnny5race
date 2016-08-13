552750a50b7fb6a06836128ca8c91967695af809
חן עידן < >
2016-08-13 03:55:45 +0300

    AJAX works with correct parms Some design (background, animated button) Click/tap event works

--- client/scripts/game.js
+++ client/scripts/game.js
@@ -1,14 +1,20 @@
+var choice=Math.random();
+var team;
+if(choice>0.5){
+	 team="B";
 
 if(Math.random()>0.5){
 	document.getElementById("team").innerHTML = "Your team is: A";
       }
  else{
+ 	team="A";
  	document.getElementById("team").innerHTML = "Your team is: B";
 
  }
  console.log("hello");
   $(".adder").click(function(){
        $.ajax({url: "http://dev.galiaba.com/chen.php",
+       	   data:{"team":team},
        		success: function(result){
            		$("#team").html(result);
         	}});
