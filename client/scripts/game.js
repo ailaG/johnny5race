3d789ce2ec4de132d65399699ce6738c60f19209
חן עידן < >
2016-08-13 03:58:19 +0300

    fix to prev

--- client/scripts/game.js
+++ client/scripts/game.js
@@ -2,16 +2,13 @@ var choice=Math.random();
 var team;
 if(choice>0.5){
 	 team="B";
-
-if(Math.random()>0.5){
-	document.getElementById("team").innerHTML = "Your team is: A";
       }
  else{
  	team="A";
- 	document.getElementById("team").innerHTML = "Your team is: B";
 
  }
- console.log("hello");
+
+ 
   $(".adder").click(function(){
        $.ajax({url: "http://dev.galiaba.com/chen.php",
        	   data:{"team":team},
